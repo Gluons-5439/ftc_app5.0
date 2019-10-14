44dbf57d4ee68562409c73f29ecdec8e32587b6c
BobbyJonesSr <clayhuang158@gmail.com>
2019-10-14 16:39:38 -0500

    Added motor info in Hardware

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Hardware.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Hardware.java
@@ -3,63 +3,81 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.hardware.bosch.BNO055IMU;
 import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
 public class Hardware {
 
-
-
-    DcMotor frontRight;         //Hub 3 Motor 0
-    DcMotor frontLeft;          //Hub 3 Motor 1
-    DcMotor backRight;          //Hub 3 Motor 2
-    DcMotor backLeft;           //Hub 3 Motor 3
+    // DEVICES
+    DcMotor frontRight;
+    DcMotor frontLeft;
+    DcMotor backRight;
+    DcMotor backLeft;
 
     private BNO055IMU imu;
-
-    DcMotor hangLift;           //Hub 2 Motor 1 (top) AND GoBilda 5202 Series Yellow
-    CRServo markerDrop;         // Hub 3 Servo 0
-    Servo latch;               //Hub 3 Servo 1
-
 
     HardwareMap hwMap;
     private ElapsedTime period = new ElapsedTime();
 
 
-        public Hardware(){
-
-        }
-
-
-        public void init (HardwareMap ahwMap){
-            hwMap = ahwMap;
-            initDevices();
-            initMotorSettings();
-            initDefaultPosition();
-        }
+    public Hardware(){
+        // Empty because we have nothing to initialise when creating a new object.
+    }
 
 
-        private void initDevices () {
-            // Initialise all parts connected to the expansion hubs
-        }
+    public void init(HardwareMap ahwMap) {
+        hwMap = ahwMap;
+        initDevices();
+        initMotorSettings();
+        initDefaultPosition();
+    }
 
-        private void initMotorSettings () {
-            // Set motor Mode and Direction
 
+    private void initDevices() {
+        // Initialise all parts connected to the expansion hubs
+        imu = hwMap.get(BNO055IMU.class, "gyro");
+        BNO055IMU.Parameters param = new BNO055IMU.Parameters();
+        param.calibrationDataFile = "BNO055IMUCalibration.json";
+        param.loggingEnabled = true;
+        param.loggingTag = "IMU";
+        param.mode = BNO055IMU.SensorMode.IMU;
+        imu.initialize(param);
 
-        }
+        frontRight = hwMap.dcMotor.get("frontRight");
+        frontLeft = hwMap.dcMotor.get("frontLeft");
+        backRight = hwMap.dcMotor.get("backRight");
+        backLeft = hwMap.dcMotor.get("backLeft");
+    }
 
-        private void initDefaultPosition () {
-            // Set default positions for motors.
-        }
+    private void initMotorSettings() {
+        // Set motor Mode and Direction
+        frontRight.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        frontLeft.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        backRight.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        backLeft.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
 
-        public void waitForTick (long periodMs) throws InterruptedException {
-            long remaining = periodMs - (long) period.milliseconds();
+        frontRight.setDirection(DcMotor.Direction.FORWARD);
+        frontLeft.setDirection(DcMotor.Direction.REVERSE);
+        backRight.setDirection(DcMotor.Direction.FORWARD);
+        backLeft.setDirection(DcMotor.Direction.REVERSE);
+    }
 
-            if (remaining > 0)
-                Thread.sleep(remaining);
+    private void initDefaultPosition() {
+        // Set default positions for motors.
+        frontRight.setPower(0);
+        frontLeft.setPower(0);
+        backRight.setPower(0);
+        backLeft.setPower(0);
+    }
 
-            period.reset();
-        }
+    public void waitForTick(long periodMs) throws InterruptedException {
+        long remaining = periodMs - (long) period.milliseconds();
+
+        if (remaining > 0)
+            Thread.sleep(remaining);
+
+        period.reset();
+    }
 }
